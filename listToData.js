3a28592a107348a53e56337876e8a6420357faa7
dounine <102535481@qq.com>
2017-05-16 02:30:50 -0500

    Update listToData.js

--- listToData.js
+++ listToData.js
@@ -1,14 +1,38 @@
-var listToData = function (obj,arryName,arryData) {
-    if(arryName&&arryData){
-        delete obj[arryName];
-        arryData.forEach(function (item,index) {
-            for(var name in item){
-                obj[arryName+'['+index+'].'+name] = item[name];
+Object.prototype.toFormData = function () {
+    var objToFormData = function (obj,obj2,sec) {
+        if(obj){
+            var count = 0;
+            for(var name in obj){
+                var val = obj[name];
+                if(val instanceof Array){
+                    val.forEach(function (item,index) {
+                        for(var name2 in item){
+                            var val2 = item[name2];
+                            if(val2 instanceof Array){
+                                val2.forEach(function (dItem,dIndex) {
+                                    objToFormData(dItem,obj,name+'['+index+'].'+name2);
+                                });
+                            }else{
+                                if((typeof val2)!='function'){
+                                    obj[name+'['+index+'].'+name2] = val2;
+                                }
+                            }
+                        }
+                    });
+                    delete obj[name];
+                }else if(sec){
+                    if((typeof val)!='function'){
+                        obj2[sec+'['+count+'].'+name] = val;
+                        count++;
+                    }
+                }
             }
-        });
+        }
     }
+    objToFormData(this);
+    return this;
 }
-var lists = [{name:'1234',age:12},{name:'33',age:22}];
+var cc = [{a:'1',b:'2'},{a:'3',b:'4'}];
+var lists = [{name:'1234',age:12},{name:'33',age:22,bb:cc}];
 var lake = {name:'hhl',age:20,ll:lists};
-listToData(lake,'ll',lists);
-console.info(lake);
+console.info(lake.toFormData());
