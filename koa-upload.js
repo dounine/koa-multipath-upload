a70969cf797ecab880185b14bf316b479fd9ba71
dounine <102535481@qq.com>
2017-05-24 01:39:37 -0500

    Update koa-upload.js

--- koa-upload.js
+++ koa-upload.js
@@ -10,3 +10,41 @@ router.post('/users', koaBody({multipart:true}),function *(next) {
     form.append('file', fs.createReadStream(file.path));
     form.append('name', file.name);
 });
+
+
+
+
+
+
+
+
+
+
+
+$scope.upload = function () {
+        var fd = new FormData();
+        var file = document.getElementById('abc').files[0];
+        fd.append('file', file);
+        $http({
+
+            method: 'POST',
+            url: '/users',
+            headers: {
+
+                'Content-Type': undefined
+
+            },
+            data: fd,
+            transformRequest: function (data, headersGetter) {
+                var formData = new FormData();
+                angular.forEach(data, function (value, key) {
+                    formData.append(key, value);
+
+                });
+                return formData;
+
+            }
+        },function (data) {
+            console.info(data);
+        });
+    }
